f7a0c9265e53d9d9c6f07856f05b2e3936adf9f8
nvng <nvngliu@gmail.com>
2013-08-31 17:33:35 +0800

    modify the copy assignment function

--- src/ccgs_common_buffer.h
+++ src/ccgs_common_buffer.h
@@ -1,4 +1,3 @@
-
 #ifndef _CCGS_COMMON_BUFFER_H_
 #define _CCGS_COMMON_BUFFER_H_
 
@@ -6,92 +5,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define     MAX_BUF     4096
+#define     MAX_BUF     (4096)
 
 class CommBuf 
 {
 public:
-    CommBuf (size_t nSize = MAX_BUF)
-    {
-        buffSize  = nSize;
-        dataPtr   = malloc (buffSize);
-        socketFd  = 0;
-        buffId    = 0;
-    }
+	CommBuf (size_t nSize = MAX_BUF)
+		: buffSize (nSize)
+		  , socketFd ()
+		  , buffId ()
+	{
+		dataPtr = malloc (buffSize);
+	}
 
-    ~CommBuf ()
-    {
-        if  (dataPtr != NULL)
-        {
-            free (dataPtr);
-            dataPtr = NULL;
-        }
-    }
+	~CommBuf ()
+	{
+		if  (dataPtr != NULL)
+		{
+			free (dataPtr);
+			dataPtr = NULL;
+		}
+	}
 
-    CommBuf (CommBuf& b)
-    {
-        buffSize = b.buffSize;
-        this->dataPtr = malloc (buffSize);
-        (void) memcpy (this->dataPtr, b.dataPtr, buffSize);
-    }
+	CommBuf (CommBuf& rhs)
+		: buffSize (rhs.buffSize)
+		  , usedSize (rhs.usedSize)
+		  , socketFd (rhs.socketFd)
+		  , buffId (rhs.buffId)
+	{
+		dataPtr = malloc (buffSize);
+		(void) memcpy (dataPtr, rhs.dataPtr, buffSize);
+	}
 
-    void* Data ()
-    {
-        return (dataPtr);
-    }
+	void* Data ()
+	{
+		return (dataPtr);
+	}
 
-    size_t SetSize (size_t n)
-    {
-        return (usedSize = n);
-    }
+	size_t SetSize (size_t n)
+	{
+		return (usedSize = n);
+	}
 
-    size_t Size ()
-    {
-        return (buffSize);
-    }
+	size_t Size ()
+	{
+		return (buffSize);
+	}
 
-    size_t UsedSize ()
-    {
-        return (usedSize);
-    }
+	size_t UsedSize ()
+	{
+		return (usedSize);
+	}
 
-    void Reset ()
-    {
-        (void) memset (dataPtr, 0x00, buffSize);
-    }
+	void Reset ()
+	{
+		(void) memset (dataPtr, 0x00, buffSize);
+	}
 
-    int SocketFd ()
-    {
-        return socketFd;
-    }
+	int SocketFd ()
+	{
+		return socketFd;
+	}
 
-    void SetSocket (int fd)
-    {
-        socketFd = fd;
-    }
+	void SetSocket (int fd)
+	{
+		socketFd = fd;
+	}
 
-    int GetId ()
-    {
-        return (buffId);
-    }
+	int GetId ()
+	{
+		return (buffId);
+	}
 
-    void SetId (int id)
-    {
-        buffId = id;
-    }
+	void SetId (int id)
+	{
+		buffId = id;
+	}
 
-    CommBuf& operator= (CommBuf& b)
-    {
-        (void) memcpy (this->dataPtr, b.Data (), buffSize);
-        return (*this);
-    }
+	CommBuf& operator= (CommBuf& rhs)
+	{
+		if (this != &rhs)
+		{
+			(void) memcpy (dataPtr, rhs.Data (), buffSize);
+			buffSize = rhs.buffSize;
+			usedSize = rhs.usedSize;
+			socketFd = rhs.socketFd;
+			buffId = rhs.buffId;
+
+			return (*this);
+		}
+	}
 
 public:
-    void*   dataPtr;
-    size_t  buffSize;
-    size_t  usedSize;
-    int     socketFd;
-    int     buffId;
+	void*   dataPtr;
+	size_t  buffSize;
+	size_t  usedSize;
+	int     socketFd;
+	int     buffId;
 };
 
 #endif  // _CCGS_COMMON_BUFFER_H
